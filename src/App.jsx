bd3af7d64e5931d41bd13143ce09b3fd8e427ac7
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-25 11:40:31 +0100

    products page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/Home'
+import Product from './pages/Product'
 
 const router = createBrowserRouter([
   { path: '/', element:<HomePage/>},
-  {}
+  {path:'/products', element: <Product/>}
 ])
 function App() {
  
