98118edde0cf575533c523233dfeb4b192aca18f
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-25 11:56:40 +0100

    added link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>My HomePage</div>
-  )
+    <>
+      <h1>My HomePage</h1>
+      <p>Go to <Link to='/products'> the list of products </Link> </p>
+    </>
+  );
 }
 
 export default HomePage
