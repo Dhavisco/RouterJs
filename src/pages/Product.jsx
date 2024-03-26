a093d81759f551566e5dac6892d1da59c94ef0a9
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-26 15:51:37 +0100

    product detail

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,8 +2,15 @@
 
 const Product = () => {
   return (
-    <div>Product</div>
-  )
+    <>
+      <h1>The Product Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 }
 
 export default Product
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,9 @@
+
+
+const ProductDetailsPage = () => {
+  return (
+    <h1>Product Details!</h1>
+  )
+}
+
+export default ProductDetailsPage
